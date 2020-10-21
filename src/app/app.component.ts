4fd0f4dc7c26c6e6d41cedbffb5068ad548c1475
hoangviet1801 <zipra91@gmail.com>
2020-10-21 11:57:10 +0700

    Testing project (21/10)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,6 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.router.navigateByUrl('/food-choice');
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
