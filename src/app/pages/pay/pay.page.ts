5bef2f76eefca1513840be979764b47173f64755
FuLygon <notfulygon.fgn@gmail.com>
2020-10-23 18:57:54 +0700

    // test test //
    Thấy như lone thì revert luôn =))), còn style đc thì style lại thử xem

--- src/app/pages/pay/pay.page.ts
+++ src/app/pages/pay/pay.page.ts
@@ -8,13 +8,26 @@ import { TicketInfoService } from 'src/app/services/ticket-info.service';
   styleUrls: ['./pay.page.scss'],
 })
 export class PayPage implements OnInit {
-
+  method:Array<string> = ['Visa & Mastercard','Nội địa','MoMo','Trực tiếp'];
+  colorName: string;
   constructor(
     private navCtrl: NavController,
     public ticketInfo: TicketInfoService
   ) { }
+  
+  switchMethod(index: number){
+    for(let i = 0; i < 4; i++){
+      (document.getElementById(`method-${i}`) as HTMLScriptElement).style.backgroundColor = this.colorName;
+    }
+    (document.getElementById(`method-${index}`) as HTMLScriptElement).style.backgroundColor = 'rgb(230 34 64 / 82%)';
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.colorName = (document.getElementById('method-0') as HTMLScriptElement).style.backgroundColor;
+    (document.getElementById('method-0') as HTMLScriptElement).style.backgroundColor = 'rgb(230 34 64 / 82%)';
   }
 
   navigate(page){
