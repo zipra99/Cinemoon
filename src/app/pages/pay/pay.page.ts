e3a12384c0d7695e26f51892694074a25dffe1a7
hoangviet1801 <zipra91@gmail.com>
2020-10-24 18:35:46 +0700

    Ticket Information ♑

--- src/app/pages/pay/pay.page.ts
+++ src/app/pages/pay/pay.page.ts
@@ -18,7 +18,8 @@ export class PayPage implements OnInit {
     '../../../assets/icon/payment/noidia.png', 
     '../../../assets/icon/payment/momo.png', 
     '../../../assets/icon/payment/tructiep.png'];
-  colorName: string;
+  backgroundColor: string;
+  borderColor: string;
   movieDetail: string[];
   userInfo: any = {};
 
@@ -36,9 +37,11 @@ export class PayPage implements OnInit {
 
   switchMethod(index: number) {
     for (let i = 0; i < 4; i++) {
-      (document.getElementById(`method-${i}`) as HTMLScriptElement).style.backgroundColor = this.colorName;
+      (document.getElementById(`method-${i}`) as HTMLScriptElement).style.borderColor = this.borderColor;
+      (document.getElementById(`method-${i}`) as HTMLScriptElement).style.backgroundColor = this.backgroundColor;
     }
-    (document.getElementById(`method-${index}`) as HTMLScriptElement).style.backgroundColor = 'rgb(230 34 64 / 82%)';
+    (document.getElementById(`method-${index}`) as HTMLScriptElement).style.borderColor = '#ff0000db';
+    (document.getElementById(`method-${index}`) as HTMLScriptElement).style.backgroundColor = '#fb463659';
   }
 
   ngOnInit() {
@@ -46,8 +49,10 @@ export class PayPage implements OnInit {
   }
 
   ngAfterViewInit() {
-    this.colorName = (document.getElementById('method-0') as HTMLScriptElement).style.backgroundColor;
-    (document.getElementById('method-0') as HTMLScriptElement).style.backgroundColor = 'rgb(230 34 64 / 82%)';
+    this.borderColor = (document.getElementById('method-0') as HTMLScriptElement).style.borderColor;
+    this.backgroundColor = (document.getElementById('method-0') as HTMLScriptElement).style.backgroundColor;
+    (document.getElementById('method-0') as HTMLScriptElement).style.borderColor = '#ff0000db';
+    (document.getElementById('method-0') as HTMLScriptElement).style.backgroundColor = '#fb463659';
   }
 
   navigate(page) {
@@ -82,9 +87,6 @@ export class PayPage implements OnInit {
     }
   }
 
-  test(i: Number) {
-    alert(i);
-  }
   async btnNext() {
     if(this.userInfo.userFullName && this.userInfo.userEmail) {
       this.ticketInfo.userInfo = this.userInfo;
